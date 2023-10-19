ed5df11285b6977145d9de8b3b547bf6628f2c9e
unknown <wisnukomara89@gmail.com>
2023-10-19 10:13:54 +0700

    Tugas Agama

---
+++ HEHEHE.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	cout<<"Masukan Nama Pacarmu : ";
+	string nama;
+	cin>>nama;
+	cout << "Hallooo, " << nama << "! I LOVE YOUUU EMESCUUUUU :*\n";  
+	
+	return 0;
+	
+}
